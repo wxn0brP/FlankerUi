07c763f860c28da2de0639ddfe5da97d3eb2fa4e
wxn0brP <toja.filiper@gmail.com>
2025-08-14 02:20:33 +0200

    feat: exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./types";
 import * as utils from "./utils";
 export * from "./component";
 export * as storeUtils from "./storeUtils";
+export * as reactStore from "./reactive";
 
 globalThis.lo = console.log;
 
